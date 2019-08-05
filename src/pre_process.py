eef9ed486e6b863130498a54f2514d2d0151cf28
Tong Wu <tongwu.github@outlook.com>
2019-08-05 22:15:21 +0800

    0805

--- src/pre_process.py
+++ src/pre_process.py
@@ -3,6 +3,24 @@ import os
 from config import data_folder
 
 raw_data_file = os.path.join(data_folder, 'raw_data.csv')
-raw_data = pd.read_csv(raw_data_file)
 
-print(raw_data)
+def list_stations():
+    df = pd.read_csv(raw_data_file)
+    return [c for (c, g) in df.groupby(['STCDT'])]
+
+def extract_station_data(id: int):
+    raw_data = pd.read_csv(raw_data_file)
+    print(raw_data.dtypes)
+    return raw_data[raw_data['STCDT'] == id]
+
+def generate_date_files():
+    stations = list_stations()
+    for s in stations:
+        df = extract_station_data(s)
+        csv_file = os.path.join(data_folder, '%d.csv' % s)
+        df.to_csv(csv_file, index = False)
+
+
+
+if (__name__ == '__main__'):
+    generate_date_files()
